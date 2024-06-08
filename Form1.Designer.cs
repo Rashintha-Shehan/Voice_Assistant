ed82dc8bfce281d84656081209e65279b0ba5850
Rashintha Shehan <143130147+Rashintha-Shehan@users.noreply.github.com>
2024-06-08 20:01:48 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace VoiceRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LstCommand = new System.Windows.Forms.ListBox();
+            this.TimeSpeek = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LstCommand
+            // 
+            this.LstCommand.BackColor = System.Drawing.Color.Navy;
+            this.LstCommand.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.LstCommand.FormattingEnabled = true;
+            this.LstCommand.ItemHeight = 16;
+            this.LstCommand.Location = new System.Drawing.Point(555, 5);
+            this.LstCommand.Name = "LstCommand";
+            this.LstCommand.Size = new System.Drawing.Size(293, 596);
+            this.LstCommand.TabIndex = 0;
+            // 
+            // TimeSpeek
+            // 
+            this.TimeSpeek.Enabled = true;
+            this.TimeSpeek.Interval = 1000;
+            this.TimeSpeek.Tick += new System.EventHandler(this.TimeSpeek_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(548, 596);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(850, 607);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LstCommand);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LstCommand;
+        private System.Windows.Forms.Timer TimeSpeek;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace VoiceRecognition
+{
+    public partial class Form1 : Form
+    {
+        private readonly SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        private readonly SpeechSynthesizer Mary = new SpeechSynthesizer();
+        private readonly SpeechRecognitionEngine startlistening = new SpeechRecognitionEngine();
+        private readonly Random rnd = new Random();
+        private int RecTimeOut = 0;
+        private DateTime lastHelloTime = DateTime.MinValue;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"defaultCommand.txt")))));
+            _recognizer.SpeechRecognized += Default_SpeechRecognized;
+            _recognizer.SpeechDetected += _recognizer_SpeechRecognized;
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            startlistening.SetInputToDefaultAudioDevice();
+            startlistening.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"defaultCommand.txt")))));
+            startlistening.SpeechRecognized += startlistening_SpeechRecognized;
+        }
+
+        private void Default_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text.ToLower();
+
+            switch (speech)
+            {
+                case "hello":
+                    if ((DateTime.Now - lastHelloTime).TotalSeconds > 5)
+                    {
+                        Mary.SpeakAsync("Hello! How can I help you?");
+                        lastHelloTime = DateTime.Now;
+                    }
+                    break;
+                case "how are you":
+                    Mary.SpeakAsync("I'm doing well, thank you for asking!");
+                    break;
+                case "what is the time now":
+                    Mary.SpeakAsync(DateTime.Now.ToString("h mm tt"));
+                    break;
+                case "date":
+                    Mary.SpeakAsync(DateTime.Now.ToString("MMMM d, yyyy"));
+                    break;
+                case "month":
+                    Mary.SpeakAsync(DateTime.Now.ToString("MMMM"));
+                    break;
+                case "year":
+                    Mary.SpeakAsync(DateTime.Now.Year.ToString());
+                    break;
+                case "wake up":
+                    // Check for similar phrases
+                    if (speech.Contains("wake up"))
+                    {
+                        startlistening.RecognizeAsyncCancel();
+                        Mary.SpeakAsync("Yes, I'm here.");
+                        _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                    }
+                    break;
+                case "stop talking":
+                    Mary.SpeakAsyncCancelAll();
+                    Mary.SpeakAsync("Yes sir.");
+                    break;
+                case "stop listening":
+                    Mary.SpeakAsync("Okay. Just call me if you need anything.");
+                    _recognizer.RecognizeAsyncCancel();
+                    startlistening.RecognizeAsync(RecognizeMode.Multiple);
+                    break;
+                case "show commands":
+                    DisplayCommands();
+                    break;
+                case "hide commands":
+                    LstCommand.Visible = false;
+                    break;
+                default:
+                    Mary.SpeakAsync("Sorry, I didn't understand. Can you please repeat?");
+                    break;
+            }
+        }
+
+        private void startlistening_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text.ToLower();
+
+            if (speech == "wake up")
+            {
+                startlistening.RecognizeAsyncCancel();
+                Mary.SpeakAsync("Yes, I'm here.");
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+        private void _recognizer_SpeechRecognized(object sender, SpeechDetectedEventArgs e)
+        {
+            RecTimeOut = 0;
+        }
+
+        private void DisplayCommands()
+        {
+            string[] commands = File.ReadAllLines(@"defaultCommand.txt");
+            LstCommand.Items.Clear();
+            LstCommand.SelectionMode = SelectionMode.None;
+            LstCommand.Visible = true;
+            foreach (string command in commands)
+            {
+                LstCommand.Items.Add(command);
+            }
+        }
+
+        private void TimeSpeek_Tick(object sender, EventArgs e)
+        {
+            if (RecTimeOut == 10)
+            {
+                _recognizer.RecognizeAsyncCancel();
+            }
+            else if (RecTimeOut == 11)
+            {
+                TimeSpeek.Stop();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+                RecTimeOut = 0;
+            }
+        }
+    }
+}
